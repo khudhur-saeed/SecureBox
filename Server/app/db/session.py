bfc06172d88a24b62a95ad5b63bedd06b6d8f3f1
khudhur <khedrmosaid@gmail.com>
2026-08-09 03:31:09 +0300

    chore: initial commit

---
+++ Server/app/db/session.py
@@ -0,0 +1,32 @@
+import os
+from app.core.config import settings
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+DATABASE_URL = os.getenv("DATABASE_URL", "my local database url")
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=True,
+    future=True
+)
+
+
+async_session_maker = async_sessionmaker(
+   bind=engine,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
+
+async def get_async_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
